65fff36ed0a228b2cc98512be75b977c4e9f70f3
zmlinebaugh04 <zmlinebaugh@students.unwsp.edu>
2022-10-27 20:39:47 -0500

    Horse

---
+++ src/DemoHorse.java
@@ -0,0 +1,22 @@
+
+public class DemoHorse{
+	public static void main(String args[]) {
+		Horse horse1 = new Horse(null, null, 0);
+		Horse horse2 = new Horse(null,null, 0);
+		
+		horse1.setName("Bruh");
+		horse1.setColor("Orange");
+		horse1.setBirthYear(2013);
+		
+		horse2.setName("Bruhdetee");
+		horse2.setName("Pink");
+		horse2.setBirthYear(2018);
+		
+		System.out.print(horse2.getBirthYear());
+		
+		
+	}
+	
+	
+
+}
---
+++ src/RaceHorse.java
@@ -0,0 +1,16 @@
+
+public class RaceHorse extends Horse {
+	private int races;
+	
+	public int getRaces() {return races;}
+	
+	public void setRaces(int race) {this.races = race;}
+	
+	public RaceHorse(String name, String color, int birthYear, int races) {
+		super(name, color, birthYear);
+		// TODO Auto-generated constructor stub
+		
+	
+	}
+
+}
